a3623539951b7d7619a9fbce2efdc81d11cff955
Bo4aroff <104056673+Bo4aroff@users.noreply.github.com>
2022-12-27 21:25:16 +0300

    Update source.py

--- source.py
+++ source.py
@@ -30,8 +30,8 @@ def generate_excel_download_link(df_2):
 #         step_two = {app_store}
 
 
-g_reviews = reviews_all(
-        'ru.drclinics.app.docnear',
+g_motherandchild = reviews_all(
+        'ru.dmo.motherandchild',
         sleep_milliseconds=0, # defaults to 0
         lang='ru', # defaults to 'en'
         country='us', # defaults to 'us'
@@ -46,7 +46,7 @@ g_df2.insert(loc=0, column='source', value='Google Play')
 g_df2.insert(loc=3, column='review_title', value=None)
 
 
-a_reviews = AppStore('ru', '1293844621')
+a_motherandchild = AppStore('ru', '1365552171')
 a_reviews.review()
 
 
